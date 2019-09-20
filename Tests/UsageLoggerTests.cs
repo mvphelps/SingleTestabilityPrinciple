7f56013198bbc7f09ff1d71ead48f58591b2c115
Michael Phelps <mvphelps@users.noreply.github.com>
2019-09-19 19:03:00 -0500

    Add presentation

--- Tests/UsageLoggerTests.cs
+++ Tests/UsageLoggerTests.cs
@@ -10,29 +10,6 @@ namespace Tests
         private string _logFileName = "testlog.txt";
         private IUsageLogger _usageLogger;
 
-        [Test]
-        public void LogError()
-        {
-            _usageLogger.RecordError("bad things happened");
-
-            AssertFileHasContents("F: bad things happened\r\n");
-        }
-
-        [Test]
-        public void LogSuccess()
-        {
-            _usageLogger.RecordSuccess(1, 2, 3);
-
-            AssertFileHasContents("S: 1+2=3\r\n");
-        }
-
-        private void AssertFileHasContents(string expected)
-        {
-            var actualContents = File.ReadAllText(_logFileName);
-            Assert.AreEqual(expected, actualContents);
-
-        }
-
         [SetUp]
         public void SetUp()
         {
@@ -49,7 +26,27 @@ namespace Tests
             }
         }
 
-        
+        private void AssertFileHasContents(string expected)
+        {
+            var actualContents = File.ReadAllText(_logFileName);
+            Assert.AreEqual(expected, actualContents);
+        }
+
+        [Test]
+        public void LogError()
+        {
+            _usageLogger.RecordError("bad things happened");
+
+            AssertFileHasContents("F: bad things happened\r\n");
+        }
+
+        [Test]
+        public void LogSuccess()
+        {
+            _usageLogger.RecordSuccess(1, 2, 3);
+
+            AssertFileHasContents("S: 1+2=3\r\n");
+        }
     }
 
     
